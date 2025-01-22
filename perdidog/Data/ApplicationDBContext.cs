c6c684cccda67c4c930fadc2c47ca67772c317f2
DonWirson <twilliamson.valdes@gmail.com>
2025-01-22 20:33:15 -0300

    chore(LostPet): :seedling: updated seeder

--- perdidog/Data/ApplicationDBContext.cs
+++ perdidog/Data/ApplicationDBContext.cs
@@ -21,13 +21,12 @@ namespace perdidog.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
 
             //Dummy Data    
             var animalTypes = new List<AnimalType>(){
-                new AnimalType {Id = Guid.Parse("e1f95dcb-0103-47f6-9ddb-dad7dae65365"),Name = "Cat" },
-                new AnimalType {Id = Guid.Parse("64d6b2f4-77a3-46d8-91be-fc36ab0103b9"),Name = "Dog"},
-                new AnimalType {Id = Guid.Parse("b8851d86-8193-48a5-9561-458b82b16f1e"),Name = "Snake"},
+                new AnimalType {Id = 1,Name = "Cat" },
+                new AnimalType {Id = 2,Name = "Dog"},
+                new AnimalType {Id = 3,Name = "Snake"},
             };
 
             var genders = new List<Gender>() {
@@ -36,10 +35,10 @@ namespace perdidog.Data
             };
 
             var lostPets = new List<LostPet>() { 
-                new LostPet {Id = Guid.Parse("8b5849f8-554b-4057-bb90-9ba29aba9a1a") ,Name = "Cabezon",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId = Guid.Parse("64D6B2F4-77A3-46D8-91BE-FC36AB0103B9"),GenderId = 1},
-                new LostPet {Id = Guid.Parse("dfae5930-af55-4aa3-8c11-f141fd75ce69") ,Name = "Cabezona",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId= Guid.Parse("64D6B2F4-77A3-46D8-91BE-FC36AB0103B9"),GenderId = 2},
-                new LostPet {Id = Guid.Parse("22e71034-37e2-44ad-b31e-928b1f431d45") ,Name = "Lucky",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId= Guid.Parse("64D6B2F4-77A3-46D8-91BE-FC36AB0103B9"),GenderId = 1},
-                new LostPet {Id = Guid.Parse("8b06fc47-666f-4685-8ec0-d5793b9be883") ,Name = "",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId= Guid.Parse("E1F95DCB-0103-47F6-9DDB-DAD7DAE65365"),GenderId = 1},
+                new LostPet {Id = 1 ,Name = "Cabezon",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId = 2,GenderId = 1},
+                new LostPet {Id = 2 ,Name = "Cabezona",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId=1,GenderId = 2},
+                new LostPet {Id = 3 ,Name = "Lucky",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId= 1,GenderId = 1},
+                new LostPet {Id = 4,Name = "",ReportDate=DateTime.Now,IsActive=true,AnimalTypeId= 2,GenderId = 1},
             };
 
             //Seeder
